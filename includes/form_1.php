177ff21ba202b22f0fdf6c2e54a7571db3e8e3f2
LE NOACH <loubna.lenoach@bnpparibas.com>
2023-10-27 18:39:49 +0200

    contact plus dinfos

--- includes/form_1.php
+++ includes/form_1.php
@@ -1,7 +1,10 @@
 <?php	
-	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0||
+	!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
 	{
+		echo "Veuillez remplir les informations obligatoires";
 		return false;
+		
 	}
 	
 	$name = $_POST['name'];
@@ -9,16 +12,18 @@
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'loubnalenoach@gmail.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from your website";
 	$email_body = "You have received a new message. \n\n".
 				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
-	$headers .= "Reply-To: $input_504";	
+	$headers .= "From: contact@agencelapanthere.com\n";
+	$headers .= "Reply-To: $email";	
 	
-	mail($to,$email_subject,$email_body,$headers); // Post message
+	//mail($to,$email_subject,$email_body,$headers); // Post message
+	$success = mail($to,$email_subject,$email_body,$headers) 
+	
 	return true;			
 ?>
